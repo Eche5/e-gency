343a3f7eac142ed768555585dc6a0c0a719c0a01
Eche5 <111073744+Eche5@users.noreply.github.com>
2023-09-22 15:21:41 +0100

    update mainjs

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,12 @@ import { AuthProvider } from "./context/AuthenticationContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
